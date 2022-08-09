11d16c1631892e63c8c4db8de338e9abf1a27ffc
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-08-09 20:06:20 -0300

    Estrutura Inicial

---
+++ api/user/datasource/user.js
@@ -0,0 +1,12 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+export class UsersAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'http://localhost:3000';
+    }
+
+    async getUsers() {
+        return this.get('/users');
+    }
+}
